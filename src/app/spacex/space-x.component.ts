15c3d9868d7a43e8fae2fdab050b46492d28038e
Ayush Shekhar <62713321+whereshouldwestart@users.noreply.github.com>
2021-11-13 23:38:49 +0530

    Create space-x.component.ts

---
+++ src/app/spacex/space-x.component.ts
@@ -0,0 +1,27 @@
+h1
+{
+  padding-top: 30px;
+  padding-left: 48%;
+  font-size: 25px;
+  color: #D92121;
+  font-family: Papyrus;
+}
+table{
+  margin-left: auto;
+  margin-right: auto;
+}
+#mission {
+  font-family: Comic Sans MS;
+  position: center;
+  border-collapse: collapse;
+}
+
+#mission td, #mission th {
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: center;
+}
+
+#mission tr:nth-child(even){background-color: #f2f2f2;}
+
+#mission tr:hover {background-color: #ddd;}
